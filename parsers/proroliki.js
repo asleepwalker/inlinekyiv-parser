cd60a297a0d91e3d2f8c6c590ac09d8b33b30b0a
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2016-08-15 00:17:16 +0300

    Add proroliki market parser

---
+++ parsers/proroliki.js
@@ -0,0 +1,17 @@
+module.exports = function($) {
+	var items = [];
+	$('.cat-item').each(function(i, el) {
+		var $el = $(this);
+		var name = $el.find('h4 a').text().trim();
+		var price = $el.find('.price').text().trim();
+		var url = $el.find('h4 a').attr('href');
+		if (name && price && url) {
+			items.push({
+				name: name,
+				price: price.replace(/[^\d.,]/g, ''),
+				url: url
+			});
+		}
+	});
+	return items;
+};
