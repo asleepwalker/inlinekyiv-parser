b507eb89587912329fa2274b0b5f8bb61b7f5faf
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2016-08-14 19:03:39 +0300

    Change database to MySQL (instead of MongoDB)

--- parse.js
+++ parse.js
@@ -1,55 +1,56 @@
 var request = require('request');
 var url = require('url');
 var cheerio = require('cheerio');
-var mongoose = require('mongoose');
+var mysql = require('mysql');
 
-mongoose.connect('mongodb://localhost/inlinekyiv');
-var db = mongoose.connection;
+var dbconfig = require('./dbconfig.json')
+var connection = mysql.createConnection(dbconfig);
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('connected', function () {
-	var sourceModel = require('./models/source');
-	var itemModel = require('./models/item');
+connection.connect();
+connection.on('error', function(err) { console.log(err); });
 
-	sourceModel.findOne().sort('lastupdate').exec(function (err, source) {
-		if (err) return console.error(err);
+// Get the oldest data source
+connection.query('select * from as_sources order by lastupdate limit 0,1', function (err, sources) {
+	if (err) return console.error(err);
 
-		source.lastupdate = Date.now();
-		source.save();
-		
-		var parser = require('./parsers/' + source.service);
-		request({
-			uri: source.endpoint,
-			method: source.method,
-			form: source.payload
-		}, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var items = parser(cheerio.load(body));
-				addSourceData(items, source);
-				Promise.all(items.map(function(item, index) {
-					return new Promise(function(resolve, reject) {
-						itemModel.update({ url: item.url }, { $set: item }, { upsert: true }, function(err) {
-							return !err ? resolve(item.name) : reject(err);
-						});
-					});
-				})).then(function() {
-					console.log(items);
-					mongoose.disconnect();
-				});
-			}
-		});
+	var source = sources[0];
+
+	// Move to the end of list
+	connection.query('update as_sources set lastupdate = NOW() where id = ' + source.id);
+
+	var parser = require('./parsers/' + source.service);
+	request({
+		uri: source.endpoint,
+		method: source.method,
+		form: source.payload ? JSON.parse(source.payload) : undefined
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var items = parser(cheerio.load(body));
+			upsertItems(items, source)
+				.then(function() { connection.end(); });
+		}
 	});
 });
 
-function addSourceData(items, source) {
+function upsertItems(items, source) {
 	var sourceUrl = url.parse(source.endpoint);
 	var basePath = sourceUrl.protocol + '//' + sourceUrl.host + '/';
-	return items.map(function(item) {
-		item.url = url.resolve(basePath, item.url);
-		item.market = source.market;
-		item.endpoint = source.endpoint;
-		item.currency = source.currency;
-		item.lastupdate = Date.now();
-		return item;
-	});
+	return Promise.all(items.map(function(item, index) {
+		return new Promise(function(resolve, reject) {
+			var query = 'insert into as_items ( ' +
+				'market, source, name, price, currency, url, lastupdate' +
+			') values ( ' +
+				'"' + source.market + '", ' +
+				'"' + source.id + '", ' +
+				'"' + item.name + '", ' +
+				'"' + item.price + '", ' +
+				'"' + source.currency + '", ' +
+				'"' + url.resolve(basePath, item.url) + '", ' +
+				'NOW()' +
+			')';
+			connection.query(query, function(err) {
+				return !err ? resolve() : reject(err);
+			});
+		});
+	}));
 }
