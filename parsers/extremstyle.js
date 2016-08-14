e3d3a472ddd4a4c86e76b2f25b316e189cc612ce
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2016-08-15 00:40:23 +0300

    Add extremstyle market parser

---
+++ parsers/extremstyle.js
@@ -0,0 +1,17 @@
+module.exports = function($) {
+	var items = [];
+	$('.row_product').each(function(i, el) {
+		var $el = $(this);
+		var name = $el.find('.infob a').text().trim();
+		var price = $el.find('.cost').text().trim();
+		var url = $el.find('.infob a').attr('href');
+		if (name && price && url) {
+			items.push({
+				name: name,
+				price: price.replace(/[^\d.,]/g, ''),
+				url: url
+			});
+		}
+	});
+	return items;
+};
