c9842c06bae48b9fdf8cdf4294d1452cedeb1d73
Sleepwalker <mail@asleepwalker.ru>
2016-08-15 16:45:20 +0300

    Fix parsing incorrect price from rollerland market

--- parsers/rollerland.js
+++ parsers/rollerland.js
@@ -3,12 +3,16 @@ module.exports = function($) {
 	$('.item').each(function(i, el) {
 		var $el = $(this);
 		var name = $el.find('.title a').text().trim();
-		var price = $el.find('.price').html().replace(/<b>.*?<\/b>/, '').trim();
+		var price = $el.find('.price').html()
+			.replace(/&#x[^;]+;/g, '') // Remove unicode text
+			.replace(/<b>.*?<\/b>/, '') // Remove strikeout text
+			.replace(/[^\d.,]/g, '') // Remove anything but number
+			.trim();
 		var url = $el.find('.title a').attr('href');
 		if (name && price && url) {
 			items.push({
 				name: name,
-				price: price.replace(/[^\d.,]/g, ''),
+				price: price,
 				url: url
 			});
 		}
