f89f50722104929beee41d22bcfd7a6a545b6f5b
Sleepwalker <mail@asleepwalker.ru>
2016-08-15 15:47:55 +0300

    Add mirrolikov market parser

---
+++ parsers/mirrolikov.js
@@ -0,0 +1,18 @@
+module.exports = function($) {
+	var items = [];
+	$('.item:not(.net_na_sklade1)').each(function(i, el) {
+		var $el = $(this);
+		var $price = $el.find('.price .price_new').length ? $el.find('.price .price_new') : $el.find('.price');
+		var name = $el.find('.name a').text().trim();
+		var price = $price.text().replace(/<b>.*?<\/b>/, '').trim();
+		var url = $el.find('.name a').attr('href');
+		if (name && price && url) {
+			items.push({
+				name: name,
+				price: price.replace(/[^\d.,]/g, ''),
+				url: url
+			});
+		}
+	});
+	return items;
+};
